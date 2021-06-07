c5720793485d300d21cd9e264d2fd94a6d097558
capfield <82098344+capfield@users.noreply.github.com>
2021-06-07 21:32:43 +0200

    Add files via upload

---
+++ Java 1/index/index.js
@@ -0,0 +1,28 @@
+console.log("Hello Winc Academy");
+
+let name = "Tess";
+console.log(name);
+
+let sum = 7 + 78;
+console.log("sum:", sum);
+
+let sumWithStrings = "7" + "78";
+console.log("sum with strings:", sumWithStrings);
+
+let subtraction = 38 - 17;
+console.log("subtraction:", subtraction);
+
+let multiplication = 200 * 4;
+console.log("multiplication:", multiplication);
+
+let division = 4 / 2;
+console.log("division:", division);
+
+let modulus = 100 % 25;
+console.log("modulus:", modulus);
+
+let age = 25;
+console.log(typeof age);
+
+let ageAsString = "25";
+console.log(typeof ageAsString);
