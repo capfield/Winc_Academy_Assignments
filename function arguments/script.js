64fdbb4342472addf63798785f157a5f1070e7a2
capfield <82098344+capfield@users.noreply.github.com>
2021-06-09 15:58:36 +0200

    Add files via upload

---
+++ function arguments/script.js
@@ -0,0 +1,10 @@
+const play = function (item) {
+    console.log("Im playing " + item);
+    console.log("you playing " + item + " sounds good!");
+};
+
+const music = "guitar";
+const moreMusic = "piano"
+
+play(music);
+play(moreMusic);
