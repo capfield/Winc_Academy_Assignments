4e7a097d564603cd1cef16b058f6e8d140362a5a
capfield <82098344+capfield@users.noreply.github.com>
2021-06-09 14:20:45 +0200

    Add files via upload

---
+++ pizza function/index.js
@@ -0,0 +1,19 @@
+const makePizza = function (){
+    console.log ("make dough")
+    console.log ("slice up some veggies")
+    console.log ("make some sauce")
+    console.log ("put some sauce on the dough")
+    console.log ("put the veggies on top of the sauce")
+    console.log ("add some meat and grated cheese")
+}
+
+makePizza();
+
+const makeSushi = function(){
+    console.log ("boil some water for the rice")
+    console.log ("once the water is boiled, add some rice")
+    console.log ("slice up some fish")
+    console.log ("put the fish in the rice")
+}
+
+makeSushi();
