aeabe1d5a1595ef464d03c9eedfe3fe060b9942a
capfield <82098344+capfield@users.noreply.github.com>
2021-06-09 19:23:47 +0200

    Add files via upload

---
+++ three ways/script.js
@@ -0,0 +1,22 @@
+
+
+/*
+const add = function (number1, number2){
+    const sum = number1 * number2;
+    return sum;
+};
+const result
+add (100, 200);
+
+
+const add = (number1, number2) => {
+    return number1 * number2
+};
+add (100, 200);
+*/
+
+function add (number1, number2) {
+    const sum = number1 * number2;
+    return sum; };
+const result = add (100, 200);
+console.log (result)
