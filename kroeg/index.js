02a6ed08ad03960c8c5fee5bf0ff0a85035304ae
capfield <82098344+capfield@users.noreply.github.com>
2021-06-09 11:12:34 +0200

    Add files via upload

---
+++ kroeg/index.js
@@ -0,0 +1,30 @@
+const age = 25
+
+if (age > 18) {
+    document.write ("you are over 18")
+}
+
+else if (age < 18) {
+    document.write ("you are too young")
+}
+
+console.log
+
+var isFemale = false
+
+if (isFemale = true) {
+    document.write ("approved")
+}
+
+else if (isFemale = false) {
+    document.write ("not approved")
+}
+
+const driverstatus = true
+
+
+if (driverstatus = true) {
+document.write ("you may drive")}
+
+else (driverstatus = false) {
+document.write ("you may not drive") }
